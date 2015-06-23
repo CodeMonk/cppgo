628916f815714ab80fa5d10062f836fe631f9ad0
David Frascone <David.Frascone@dishdigital.com>
2015-06-23 09:56:53 -0600

    Printed out more go vars

--- main.go
+++ main.go
@@ -12,12 +12,17 @@ import (
 func main() {
 	// Connect to our c-go library, and see if we can hold the pointers.
 	handle := C.goAlloc(C.CString("Go String %d"), C.int(5))
+	goHandle := uint64(handle)
 
-	gotos := C.goToString(handle)
-
+	gotos := C.goToString(C.uint64_t(goHandle))
 	result := C.GoString(gotos)
 
-	fmt.Printf("Result: %s\n", result)
+	fmt.Println("Vars:")
+	fmt.Printf("    handle: %#v (%T)\n", handle, handle)
+	fmt.Printf("  goHandle: %#v (%T)\n", goHandle, goHandle)
+	fmt.Printf("     gotos: %#v (%T)\n", gotos, gotos)
+
+	fmt.Printf("Result:\n    %s\n", result)
 
 	C.goFree(handle)
 
